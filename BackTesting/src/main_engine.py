499ae7ccfc75bbcdb7187781892824543f590dc3
Mridul Agarwal <mridul310703@gmail.com>
2023-12-16 21:48:49 +0530

    Final Engine Added

--- BackTesting/src/main_engine.py
+++ BackTesting/src/main_engine.py
@@ -1,17 +1,15 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
-import os
-import talib
 from empyrical import max_drawdown
 
 class Static_Engine():
-    def __init__(self, log_name = None, static_trade_amount = 1000, without_transaction_cost = False, tick_sz = 5) -> None:
-        self.logs = None
-        self.each_trade_amount = static_trade_amount
+    def __init__(self, static_trade_amount = 1000, without_transaction_cost = False) -> None:
+        self.logs = None # dataframe of logs
+        self.each_trade_amount = static_trade_amount # each trade amount
         self.status = 0
         self.net_pnl = 0
-        self.max_total_loss_capacity = - 100*self.each_trade_amount
+        self.max_total_loss_capacity = - 5*self.each_trade_amount
         self.trade_pnl_lst = []
         self.net_pnl_lst = []
         self.metrics = {}
@@ -32,10 +30,6 @@ class Static_Engine():
             self.transaction_cost = 0
         self.last_trade_open_time = None
         self.trade_holding_times = []
-        self.immediate_losses = 0
-        self.immediate_profits = 0
-        self.min_time_diff = pd.Timedelta(minutes=tick_sz)
-        self.log_name = log_name
         self.close_price_lst = []
         self.open_price_lst = []
         self.trade_returns = []
@@ -49,7 +43,7 @@ class Static_Engine():
         self.open_price_lst = self.logs['open'].tolist()
         self.logs['trade_pnl'] = 0
         self.logs['trade_pnl'] = self.logs['trade_pnl'].astype('float64')
-        df = self.logs[self.logs['signal'] != 0]
+        df = self.logs[self.logs['signal'] != 0] # get only the rows where signal is not 0 for faster execution
         first_timestamp = None
         for row in df.itertuples():
             signal = int(row.signal)
@@ -77,16 +71,16 @@ class Static_Engine():
                 continue
 
             if(self.net_pnl < self.max_total_loss_capacity):
-                print(f"maa chud gayi at {timestamp}")
+                print(f"Net PnL dropped less than {self.max_total_loss_capacity} {timestamp}")
                 exit(1)
 
             trade_pnl = 0
 
-            if signal == 1:
-                if(self.status == 0):
+            if signal == 1: # Buy Signal
+                if(self.status == 0): # Initiate Long Trade
                     self.assets = self.each_trade_amount / price
                     self.last_trade_open_time = timestamp
-                elif(self.status == -1):
+                elif(self.status == -1): # Close the short trade
                     trade_pnl = self.each_trade_amount + self.assets * price
                     trade_pnl -= self.transaction_cost * self.each_trade_amount
                     self.total_transaction_cost += self.transaction_cost * self.each_trade_amount
@@ -95,20 +89,15 @@ class Static_Engine():
                     self.total_trades_closed += 1
                     trade_closed = True
                     self.trade_holding_times.append(timestamp - self.last_trade_open_time)
-                    if timestamp - self.last_trade_open_time == self.min_time_diff:
-                        if trade_pnl > 0:
-                            self.immediate_profits += trade_pnl 
-                        else:
-                            self.immediate_losses += trade_pnl 
                 else:
                     print(f"Invalid trade signal at {timestamp}")
                     continue
 
-            if signal == -1:
-                if(self.status == 0):
+            if signal == -1: # Sell Signal
+                if(self.status == 0): # Initiate Short Trade
                     self.assets = -self.each_trade_amount / price
                     self.last_trade_open_time = timestamp
-                elif(self.status == 1):
+                elif(self.status == 1): # Close the long trade
                     trade_pnl = self.assets * price - self.each_trade_amount
                     trade_pnl -= self.transaction_cost * self.each_trade_amount
                     self.total_transaction_cost += self.transaction_cost * self.each_trade_amount
@@ -117,17 +106,12 @@ class Static_Engine():
                     self.total_trades_closed += 1
                     trade_closed = True
                     self.trade_holding_times.append(timestamp - self.last_trade_open_time)
-                    if timestamp - self.last_trade_open_time == self.min_time_diff:
-                        if trade_pnl > 0:
-                            self.immediate_profits += trade_pnl
-                        else:
-                            self.immediate_losses += trade_pnl
                 else:
                     print(f"Invalid trade signal at {timestamp}")
                     continue
 
-            ## Updating status
-            self.status += signal
+            self.status += signal ## Updating status
+
             if trade_closed:
                 self.trade_pnl_lst.append(trade_pnl)
                 self.trade_returns.append(trade_pnl + self.transaction_cost * self.each_trade_amount)
@@ -146,11 +130,13 @@ class Static_Engine():
 
             self.min_net_pnl = min(self.min_net_pnl, self.net_pnl)
 
-            if(signal != 0):
-                # print(f"Trade at {timestamp} with price {price} and signal {signal}, total assets : {self.assets}, trade_pnl : {trade_pnl}, net_pnl : {self.net_pnl}")
-                continue
+            # Uncomment to see each trade
+            # if(signal != 0):
+            #     print(f"Trade at {timestamp} with price {price} and signal {signal}, total assets : {self.assets}, trade_pnl : {trade_pnl}, net_pnl : {self.net_pnl}")
+
         self.logs['net_pnl'] = self.logs['trade_pnl'].cumsum()
         self.net_pnl_lst = self.logs['net_pnl'].tolist()
+        # Closing the last unclosed trade
         trade_closed = False
         if self.assets > 0:
             trade_pnl = self.assets * close - self.each_trade_amount
@@ -202,7 +188,7 @@ class Static_Engine():
                 self.losing_trades_lst.append(trade_pnl)
                 self.num_lose_trades += 1
 
-    def plot(self) -> None:
+    def plot(self) -> None: # Plots PnL and Close Price
         fig, ax = plt.subplots()
         ax.plot(self.net_pnl_lst, label='Net PnL', color='blue')
         ax.axhline(y=0, color='black', linestyle='--')
@@ -227,7 +213,6 @@ class Static_Engine():
         lines2, labels2 = ax2.get_legend_handles_labels()
         ax.legend(lines + lines2, labels + labels2, loc='upper left')
         
-        # plt.show()
         plt.show()
 
     def get_metrics(self) -> dict:
@@ -245,13 +230,11 @@ class Static_Engine():
         self.metrics["Number of Winning Trades"] = self.num_win_trades
         self.metrics["Number of Losing Trades"] = self.num_lose_trades
         self.net_portfolio_lst = np.array(self.net_pnl_lst) + self.each_trade_amount
-        # self.metrics["Maximum Drawdown"] = np.max((1 - self.net_portfolio_lst / np.maximum.accumulate(self.net_portfolio_lst)))*100
-        self.metrics["Maximum Drawdown"] = max_drawdown(np.array(self.trade_returns)/1000)*100
+        self.metrics["Maximum Drawdown"] = np.max((1 - self.net_portfolio_lst / np.maximum.accumulate(self.net_portfolio_lst)))*100
+        # self.metrics["Maximum Drawdown"] = max_drawdown(np.array(self.trade_returns)/1000)*100
         self.metrics["Total Transaction Cost"] = self.total_transaction_cost
         self.metrics["Average Trade Holding Duration"] = np.mean([self.trade_holding_times])
         self.metrics["Maximum Trade Holding Duration"] = np.max([self.trade_holding_times])
-        self.metrics["Immediate Losses"] = self.immediate_losses
-        self.metrics["Immediate Profits"] = self.immediate_profits
         self.metrics["Annual Maximum Drawdowns"] = {}
         self.metrics["Annual Returns"] = {}
         for year, trade_returns in self.annual_trade_returns.items():
@@ -263,14 +246,14 @@ class Static_Engine():
         return self.metrics
     
 
-class Compunding_Engine():
+class Compounding_Engine():
     def __init__(self, initial_cash = 1000) -> None:
         self.logs = None
         self.initial_cash = initial_cash
         self.cash = self.initial_cash
         self.status = 0
         self.net_pnl = 0
-        self.max_total_loss_capacity = - 100*self.initial_cash
+        self.max_total_loss_capacity = -5*self.initial_cash
         self.daily_pnl_lst = []
         self.net_pnl_lst = []
         self.metrics = {}
@@ -284,13 +267,15 @@ class Compunding_Engine():
         self.losing_trades_lst = []
         self.num_win_trades = 0
         self.num_lose_trades = 0
-        self.metrics_logs = pd.DataFrame(columns=['Trade PnL', 'Net PnL'])
         self.last_bough_amt = 0
         self.last_sold_amt = 0
         self.transaction_cost = 0.0015
         self.total_transaction_cost = 0
+        self.returns_lst = []
+        self.close_price_lst = []
+        self.trade_holding_times = []
+        self.last_trade_open_time = None
         self.annual_trade_returns = dict()
-
 
 
     def add_logs(self, logs: pd.DataFrame) -> None:
@@ -298,9 +283,14 @@ class Compunding_Engine():
 
     def run(self) -> None:
         first_timestamp = None
+        self.close_price_lst = self.logs['close'].tolist()
+        self.open_price_lst = self.logs['open'].tolist()
         for row in self.logs.itertuples():
             signal = int(row.signal)
             open = row.open
+            close = row.close
+            price = close 
+
             timestamp = row.datetime
             timestamp = pd.to_datetime(timestamp)
             if first_timestamp is None:
@@ -310,8 +300,6 @@ class Compunding_Engine():
             if year_difference>=1:
                 self.annual_trade_returns[str(timestamp.year)] = list()
                 first_timestamp = timestamp
-            close = row.close
-            price = close
 
             if (signal) not in [-1, 0, 1]:
                 print(f"Invalid trade signal at {timestamp}")
@@ -322,41 +310,47 @@ class Compunding_Engine():
                 continue
 
             if(self.net_pnl < self.max_total_loss_capacity):
-                print(f"maa chud gayi at {timestamp}")
+                print(f"Net PnL dropped less than {self.max_total_loss_capacity} {timestamp}")
                 exit(1)
 
             trade_pnl = 0
 
-            if signal == 1:
-                if(self.status == 0):
+            if signal == 1: # Buy Signal
+                if(self.status == 0): # Initiate Long Trade
                     self.assets = self.cash / price
                     self.last_bough_amt = self.cash
                     self.cash = 0
-                elif(self.status == -1):
+                    self.last_trade_open_time = timestamp
+                elif(self.status == -1): # Close the short trade
                     trade_pnl = self.last_sold_amt + self.assets * price
                     trade_pnl -= self.transaction_cost*self.last_sold_amt
+                    self.returns_lst.append(trade_pnl/self.last_sold_amt)
                     self.total_transaction_cost += self.transaction_cost*self.last_sold_amt
                     self.net_pnl += trade_pnl
                     self.assets = 0
                     self.total_trades_closed += 1
                     self.cash -= self.last_sold_amt
+                    self.trade_holding_times.append(timestamp - self.last_trade_open_time)
                 else:
                     print(f"Invalid trade signal at {timestamp}")
                     continue
 
-            if signal == -1:
-                if(self.status == 0):
+            if signal == -1: # Sell Signal
+                if(self.status == 0): # Initiate Short Trade
                     self.assets = -self.cash / price
                     self.last_sold_amt = self.cash
                     self.cash += self.cash
-                elif(self.status == 1):
+                    self.last_trade_open_time = timestamp
+                elif(self.status == 1): # Close the long trade
                     trade_pnl = self.assets * price - self.last_bough_amt
                     trade_pnl -= self.transaction_cost * self.last_bough_amt
+                    self.returns_lst.append(trade_pnl/self.last_bough_amt)
                     self.total_transaction_cost += self.transaction_cost * self.last_bough_amt
                     self.net_pnl += trade_pnl
                     self.assets = 0
                     self.total_trades_closed += 1
                     self.cash = self.last_bough_amt
+                    self.trade_holding_times.append(timestamp - self.last_trade_open_time)
                 else:
                     print(f"Invalid trade signal at {timestamp}")
                     continue
@@ -380,10 +374,11 @@ class Compunding_Engine():
 
             self.min_net_pnl = min(self.min_net_pnl, self.net_pnl)
 
-            if(signal != 0):
-                # print(f"Trade at {timestamp} with price {price} and signal {signal}, total assets : {self.assets}, trade_pnl : {trade_pnl}, net_pnl : {self.net_pnl}, net_cash : {self.cash}")
-                continue
-
+            # Uncomment to see each trade
+            # if(signal != 0):
+            #     print(f"Trade at {timestamp} with price {price} and signal {signal}, total assets : {self.assets}, trade_pnl : {trade_pnl}, net_pnl : {self.net_pnl}, net_cash : {self.cash}")
+        
+        # Closing the last unclosed trade
         if self.assets > 0:
             trade_pnl = self.assets * close - self.last_bough_amt
             self.net_pnl += trade_pnl
@@ -404,6 +399,7 @@ class Compunding_Engine():
                 self.losing_trades_lst.append(trade_pnl)
                 self.num_lose_trades += 1
             self.cash += trade_pnl
+            self.trade_holding_times.append(timestamp - self.last_trade_open_time)
             
         elif self.assets < 0:
             trade_pnl = self.last_sold_amt + self.assets * close
@@ -425,17 +421,38 @@ class Compunding_Engine():
                 self.losing_trades_lst.append(trade_pnl)
                 self.num_lose_trades += 1
             self.cash += trade_pnl
+            self.trade_holding_times.append(timestamp - self.last_trade_open_time)
 
-    def plot(self)-> None:
-        plt.plot(self.net_pnl_lst)
-        plt.axhline(y=0, color='black', linestyle='--') # add this line to draw x-axis at y=0
-        plt.title("Net PnL for Compounding Approach")
-        plt.xlabel("Time")
-        plt.ylabel("Net PnL")
+    def plot(self) -> None: # Plotting the PnL and Close Price
+        fig, ax = plt.subplots()
+        ax.plot(self.net_pnl_lst, label='Net PnL', color='blue')
+        ax.axhline(y=0, color='black', linestyle='--')
+        ax.set_title("Net PnL and Close Price for Static Approach")
+        
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Net PnL")
+        
+        ax2 = ax.twinx()
+        ax2.plot(self.close_price_lst , label='Close Price', color='orange')
+        ax2.axhline(y=0, color='black', linestyle='--')
+        ax2.set_ylabel("Close Price")
+        
+        # Set xtick values using self.logs['datetime']
+        len = self.logs.shape[0] - 1
+        xticks = [0, len//4, 2*(len//4), 3*(len//4), len]  # Set xticks at the start and end
+        ax.set_xticks(xticks)
+        ax.set_xticklabels([self.logs.loc[0, 'datetime'], self.logs.loc[len//4, 'datetime'], self.logs.loc[2*(len//4), 'datetime'], self.logs.loc[3*(len//4), 'datetime'], self.logs.loc[len, 'datetime']])  # Set labels at the start and end
+            
+        # combine the legend entries from all plots
+        lines, labels = ax.get_legend_handles_labels()
+        lines2, labels2 = ax2.get_legend_handles_labels()
+        ax.legend(lines + lines2, labels + labels2, loc='upper left')
+        
         plt.show()
 
     def get_metrics(self) -> dict:
         self.metrics["Net PnL"] = self.net_pnl
+        self.metrics["Buy and Hold PnL"] = (self.close_price_lst[-1] - self.close_price_lst[0]) * self.each_trade_amount / self.close_price_lst[0]
         self.metrics["Gross Profit"] = self.gross_profit
         self.metrics["Gross Loss"] = self.gross_loss
         self.metrics["Total Trades Closed"] = self.total_trades_closed
@@ -448,7 +465,10 @@ class Compunding_Engine():
         self.metrics["Number of Winning Trades"] = self.num_win_trades
         self.metrics["Number of Losing Trades"] = self.num_lose_trades
         self.metrics["Final Cash"] = self.cash
+        self.metrics["Maximum Drawdown"] = max_drawdown(np.array(self.returns_lst))
         self.metrics["Total Transaction cost"] = self.total_transaction_cost
+        self.metrics["Average Trade Holding Duration"] = np.mean([self.trade_holding_times])
+        self.metrics["Maximum Trade Holding Duration"] = np.max([self.trade_holding_times])
         self.metrics["Annual Maximum Drawdowns"] = {}
         self.metrics["Annual Returns"] = {}
         for year, trade_returns in self.annual_trade_returns.items():
--- BackTesting/src/run_engine.py
+++ BackTesting/src/run_engine.py
@@ -1,21 +1,36 @@
-from main_engine import Static_Engine, Compunding_Engine
+from main_engine import Static_Engine, Compounding_Engine
 import pandas as pd
-import sys
 import argparse
 
-
-tstomin = {
-    '3m': 3,
-    '5m': 5,
-    '15m': 15,
-    '30m': 30,
-    '1h': 60,
-}
+def print_metrics(metrics):
+    print(f"Net PnL {metrics['Net PnL']}")
+    print(f"Buy and Hold PnL {metrics['Buy and Hold PnL']}")
+    print(f"Total Trades Closed {metrics['Total Trades Closed']}")
+    print(f"Gross Profit {metrics['Gross Profit']}")
+    print(f"Gross Loss {metrics['Gross Loss']}")
+    print(f"Largest Winning Trade {metrics['Largest Winning Trade']}")
+    print(f"Largest Losing Trade {metrics['Largest Losing Trade']}")
+    print(f"Min Net PnL {metrics['Min Net PnL']}")
+    print(f"Sharpe Ratio {metrics['Sharpe Ratio']}")
+    print(f"Average Winning Trade {metrics['Average Winning Trade']}")
+    print(f"Average Losing Trade {metrics['Average Losing Trade']}")
+    print(f"Number of Winning Trades {metrics['Number of Winning Trades']}")
+    print(f"Number of Losing Trades {metrics['Number of Losing Trades']}")
+    print(f"Total Transaction Cost {metrics['Total Transaction Cost']}")
+    print(f"Max Drawdown {metrics['Maximum Drawdown']}")
+    print(f"Maximum Trade Holding Duration {metrics['Maximum Trade Holding Duration']}")
+    print(f"Average Trade Holding Duration {metrics['Average Trade Holding Duration']}")
+    print("\nAnnual Returns:")
+    for year, annual_return in metrics["Annual Returns"].items():
+        print(f"{year}: {annual_return:.2f}%")
+    print("\nAnnual Maximum Drawdowns:")
+    for year, max_drawdown in metrics["Annual Maximum Drawdowns"].items():
+        print(f"{year}: {max_drawdown:.2f}%")
 
 def generate_results_static(args):
-    e = Static_Engine(log_name = args.logs, tick_sz=tstomin[args.ts],  without_transaction_cost=args.wtc)
+    # Make instance of static engine, add logs, run, get metrics and plot
+    e = Static_Engine(log_name = args.logs,  without_transaction_cost=args.wtc)
     df = pd.read_csv(args.logs)
-    print(len(df))
     e.add_logs(df)
     e.run()
     metrics = e.get_metrics()
@@ -24,37 +39,12 @@ def generate_results_static(args):
     print("*" + "Results for Static Method".center(58) + "*")
     print("*" + " " * 58 + "*")
     print("*"*60 + "\n")
-    print(metrics['Net PnL'])
-    print(f"Net PnL {metrics['Net PnL']}")
-    print(f"Buy and Hold PnL {metrics['Buy and Hold PnL']}")
-    print(f"Total Trades Closed {metrics['Total Trades Closed']}")
-    print(f"Gross Profit {metrics['Gross Profit']}")
-    print(f"Gross Loss {metrics['Gross Loss']}")
-    print(f"Largest Winning Trade {metrics['Largest Winning Trade']}")
-    print(f"Largest Losing Trade {metrics['Largest Losing Trade']}")
-    print(f"Sharpe Ratio {metrics['Sharpe Ratio']}")
-    print(f"Min Net PnL {metrics['Min Net PnL']}")
-    print(f"Average Winning Trade {metrics['Average Winning Trade']}")
-    print(f"Average Losing Trade {metrics['Average Losing Trade']}")
-    print(f"Number of Winning Trades {metrics['Number of Winning Trades']}")
-    print(f"Number of Losing Trades {metrics['Number of Losing Trades']}")
-    print(f"Max Drawdown {metrics['Maximum Drawdown']}")
-    print(f"Total Transaction Cost {metrics['Total Transaction Cost']}")
-    print(f"Maximum Trade Holding Duration {metrics['Maximum Trade Holding Duration']}")
-    print(f"Average Trade Holding Duration {metrics['Average Trade Holding Duration']}")
-    print(f"Immediate Losses {metrics['Immediate Losses']}" )
-    print(f"Immediate Profits {metrics['Immediate Profits']}" )
-    print(f"Net PnL {metrics['Net PnL']}")
-    print("\nAnnual Returns:")
-    for year, annual_return in metrics["Annual Returns"].items():
-        print(f"{year}: {annual_return:.2f}%")
-    print("\nAnnual Maximum Drawdowns:")
-    for year, max_drawdown in metrics["Annual Maximum Drawdowns"].items():
-        print(f"{year}: {max_drawdown:.2f}%")
+    print_metrics(metrics)
     e.plot()
 
 def generate_results_compounding(args):
-    e = Compunding_Engine()
+    # Make instance of compounding engine, add logs, run, get metrics and plot
+    e = Compounding_Engine()
     df = pd.read_csv(args.logs)
     e.add_logs(df)
     e.run()
@@ -64,26 +54,7 @@ def generate_results_compounding(args):
     print("*" + "Results for Compounding Method".center(58) + "*")
     print("*" + " " * 58 + "*")
     print("*"*60 + "\n")
-    print(f"Net PnL {metrics['Net PnL']}")
-    print(f"Total Trades Closed {metrics['Total Trades Closed']}")
-    print(f"Gross Profit {metrics['Gross Profit']}")
-    print(f"Gross Loss {metrics['Gross Loss']}")
-    print(f"Largest Winning Trade {metrics['Largest Winning Trade']}")
-    print(f"Largest Losing Trade {metrics['Largest Losing Trade']}")
-    print(f"Sharpe Ratio {metrics['Sharpe Ratio']}")
-    print(f"Min Net PnL {metrics['Min Net PnL']}")
-    print(f"Average Winning Trade {metrics['Average Winning Trade']}")
-    print(f"Average Losing Trade {metrics['Average Losing Trade']}")
-    print(f"Number of Winning Trades {metrics['Number of Winning Trades']}")
-    print(f"Number of Losing Trades {metrics['Number of Losing Trades']}")
-    print(f"Final Cash {metrics['Final Cash']}")
-    print(f"Total Transaction Cost {metrics['Total Transaction cost']}")
-    print("\nAnnual Returns:")
-    for year, annual_return in metrics["Annual Returns"].items():
-        print(f"{year}: {annual_return:.2f}%")
-    print("\nAnnual Maximum Drawdowns:")
-    for year, max_drawdown in metrics["Annual Maximum Drawdowns"].items():
-        print(f"{year}: {max_drawdown:.2f}%")
+    print_metrics(metrics)
     e.plot()
 
 def run_method(args):
@@ -96,13 +67,11 @@ def run_method(args):
     method_function = methods.get(args.method, lambda: None)
     return method_function()
 
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--logs", help="path to logs file", required=True)
     parser.add_argument('--wtc', action='store_true', help='without transaction cost')
-    parser.add_argument('--ts', help='tick size eg. 3m/5m/15m/30m/1h', required=True)
     parser.add_argument('--method',help='static, compounding or both', default='static')
     args = parser.parse_args()
     run_method(args)
